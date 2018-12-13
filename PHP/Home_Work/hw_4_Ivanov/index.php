d8c5a00fb8b5dff0c2a7fba9c4fe920d0cc6a984
unknown <jinsike123@gmail.com>
2018-12-12 16:53:54 -0800

    Доработка домашнего задание 13 декаюря ночью

--- PHP/Home_Work/hw_4_Ivanov/index.php
+++ PHP/Home_Work/hw_4_Ivanov/index.php
@@ -29,7 +29,7 @@
 
 <html>
 <head>
-	<title>Курс валют less3</title>
+	<title>Курс валют less4</title>
 	<meta charset="utf-8">
 	<meta name="description" content="Home worki lesson 3.">
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
