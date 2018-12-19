bf41003beba9976eb4a2a1856511241d56e36283
unknown <jinsike123@gmail.com>
2018-12-18 17:27:37 -0800

    Исправление домашнего задание: 1—неправильная константа, 2—неправильно стоял инкремент, 3—двойные кавычки, 4—текст за пределы php тегов.

--- PHP/Home_Work/hw_4_Ivanov/index.php
+++ PHP/Home_Work/hw_4_Ivanov/index.php
@@ -1,43 +1,40 @@
 <?php 
 	$titlePageName = 'Курс гривны';
-	$descrPage = 'Курс валют в Украине: курс доллара к гривне, курс евро и гривны. Курс валют на сегодня в Украине онлайн.
-	';
-	// const for calculation of the hryvna	
-	$DOLLAR_RATE = 27.7;
-	$EURO_RATE = 31.35;
-	$STEP = 50;
-	$UA_BEFORE = 2000;
+	$descrPage = 'Курс валют в Украине: курс доллара к гривне, курс евро и гривны. Курс валют на сегодня в Украине онлайн.';
+	// const for calculation of the hryvna
+	const DOLLAR_RATE = 27.7;
+	const EURO_RATE = 31.35;
+	const STEP = 50;
+	const UA_BEFORE = 2000;
 	// contains value hrivnia
-	$uaFrom = 1000;	
-?>	
-
+	$uaFrom = 1000;
+?>
 <html>
-<?php include_once "head.php" ?>
+<?php include_once 'head.php' ?>
 <body>
 	<div class="container">
-		<?php include_once "header.php"?>
+		<?php include_once 'header.php'?>
 		<h2 class="text-center">Курс грианы на сегодня</h2>
 		<table class="table table-bordered">
 			<thead>
 				<tr>
 					<th>Валюта в гривнах</th>
 					<th>Валюта в долларах США</th>
-					<th>Валюта в евро</th>					
+					<th>Валюта в евро</th>
 				</tr>
 			</thead>
 			<tbody>
-				<?php while ($uaFrom <= $UA_BEFORE) : ?>
-					<tr>					
-						<td><?= $uaFrom . ' грн.' ?></td>
-						<?php $uaFrom += $STEP ?>
-						<td><?= round($uaFrom / $DOLLAR_RATE,3) . ' usd.' ?></td>
-						<td><?= round($uaFrom / $EURO_RATE,3) . ' euro.' ?></td>
+				<?php while ($uaFrom <= UA_BEFORE) : ?>
+					<tr>
+						<td><?= $uaFrom ?> грн.</td>
+						<td><?= round($uaFrom / DOLLAR_RATE,3) ?> usd.</td>
+						<td><?= round($uaFrom / EURO_RATE,3) ?> euro.</td>
+						<?php $uaFrom += STEP ?>
 					</tr>
-				<?php endwhile; ?>				
+				<?php endwhile ?>
 			</tbody>
 		</table>
-
-		<?php include_once "footer.php" ?>
+		<?php include_once 'footer.php' ?>
 	</div> <!-- END container -->
 </body>
 </html>
--- PHP/Home_Work/hw_4_Ivanov/tovars.php
+++ PHP/Home_Work/hw_4_Ivanov/tovars.php
@@ -4,59 +4,57 @@
 	// Creating Array for Table Home worrk 12 dec 5:01
 	$tovars = [
 		[
-			"name" => "Fanta",
-			"price" => 8,
-			"cat" => "drink",
+			'name' => 'Fanta',
+			'price' => 8,
+			'cat' => 'drink',
 		],
 		[
-			"name" => "Coca-cola",
-			"price" => 24,
-			"cat" => "drink",
+			'name' => 'Coca-cola',
+			'price' => 24,
+			'cat' => 'drink',
 		],
 		[
-			"name" => "Sandora",
-			"price" => 18,
-			"cat" => "drink",
+			'name' => 'Sandora',
+			'price' => 18,
+			'cat' => 'drink',
 		],
-		[	"name" => "Pepsi",
-			"price" => 17,
-			"cat" => "driink",
-		],
-		[
-			"name" => "Tomato",
-			"price" => 50,
-			"cat" => "food",
+		[	'name' => 'Pepsi',
+			'price' => 17,
+			'cat' => 'driink',
 		],
 		[
-			"name" => "Potate",
-			"price" => 15,
-			"cat" => "food",
+			'name' => 'Tomato',
+			'price' => 50,
+			'cat' => 'food',
 		],
 		[
-			"name" => "Flint",
-			"price" => 4,
-			"cat" => "food",
+			'name' => 'Potate',
+			'price' => 15,
+			'cat' => 'food',
 		],
 		[
-			"name" => "Orol_B",
-			"price" => 6,
-			"cat" => "food",
+			'name' => 'Flint',
+			'price' => 4,
+			'cat' => 'food',
 		],
 		[
-			"name" => "Fish",
-			"price" => 80,
-			"cat" => "food",
+			'name' => 'Orol_B',
+			'price' => 6,
+			'cat' => 'food',
 		],
 		[
-			"name" => "Ckeice",
-			"price" => 2,
-			"cat" => "food",
+			'name' => 'Fish',
+			'price' => 80,
+			'cat' => 'food',
 		],
-	];
-	
+		[
+			'name' => 'Ckeice',
+			'price' => 2,
+			'cat' => 'food',
+		],
+	];	
 	//  discount for all tovars example: 34%
 	$discount = '10%';
-
 	$discount /= 100;
 	// code for calculation discount
 	foreach ($tovars as $key => $tovar) {
@@ -65,21 +63,20 @@
 		$priceWithDiscount = $currentPrice - $currentDiscount;
 		// change the price of the current tovar
 		$tovars[$key]['price'] = $priceWithDiscount;
-	}	
-?>	
-
+	}
+?>
 <html>
-<?php include_once "head.php" ?>
+<?php include_once 'head.php' ?>
 <body>
-	<div class="container">
-		<?php include_once "header.php"?>
-		<h2 class="text-center">Накладная для магазина Santim</h2>
-		<table class="table table-bordered">
+	<div class='container'>
+		<?php include_once 'header.php'?>
+		<h2 class='text-center'>Накладная для магазина Santim</h2>
+		<table class='table table-bordered'>
 			<thead>
 				<tr>
 					<th>Название товара</th>
 					<th>Категория</th>
-					<th>Цена за один</th>					
+					<th>Цена за один</th>
 				</tr>
 			</thead>
 			<tbody>
@@ -87,14 +84,14 @@
 					<tr>
 						<th><?= $tovar['name'] ?></th>
 						<td><?= $tovar['cat'] ?></td>
-						<td><?= $tovar['price'] . ' грн. ' ?></td>
+						<td><?= $tovar['price'] ?> грн. </td>
 					</tr>
 				<?php endforeach; ?>
 				
 				</tr>
 			</tbody>
 		</table>
-		<?php include_once "footer.php" ?>
+		<?php include_once 'footer.php' ?>
 	</div> <!-- END container -->
 </body>
 </html>
